7b8a36284ebe0b57fd3995dc3ee91f17fcc73f63
sinchana <sinchanacsinchu216@gmail.com>
2026-05-08 15:53:25 +0530

    fixed backend setup and tested api successfully

--- backend/src/main/java/com/internship/ToolApplication.java
+++ backend/src/main/java/com/internship/ToolApplication.java
@@ -1,4 +1,4 @@
-package com.internship.tool;
+package com.internship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ToolApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ToolApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ToolApplication.class, args);
+    }
 }
